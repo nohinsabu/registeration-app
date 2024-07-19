3b5700d69fa34aaed5e97ad63e514eb12f8481b5
nohinsabu01 <nohin.sabu@intern.mhtechin.com>
2024-07-19 14:35:31 +0530

    complete signin page functionality

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 
-export default function SignUp() {
+export default function Register() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
