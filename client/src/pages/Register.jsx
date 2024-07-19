35b5cd948b5f9369fd258034290a3b60d3d643b9
nohinsabu01 <nohin.sabu@intern.mhtechin.com>
2024-07-19 14:10:48 +0530

    complete register ui and functionality

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
-export default function Register() {
+
+export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate  = useNavigate();
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value
+    });
+  }
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch('/api/auth/register', 
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      }
+    );
+    const data = await res.json();
+    if (data.success === false){
+      setLoading(false);
+      setError(data.message);
+      return;
+    }
+    setLoading(false);
+    setError(null);
+    navigate('/');
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+    console.log(data);
+  }
+  
   return (
-    <div>
-      Register
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Register</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input type='text' placeholder='Username' 
+        className='border p-3 rounded-lg' id='username' onChange={handleChange} />
+        <input type='date' placeholder='Date of Birth' 
+        className='border p-3 rounded-lg' id='dob' onChange={handleChange} />
+        <input type='email' placeholder='Email' 
+        className='border p-3 rounded-lg' id='email' onChange={handleChange} />
+        <input type='password' placeholder='Password' 
+        className='border p-3 rounded-lg' id='password' onChange={handleChange} />
+        <button disabled={loading} className='bg-slate-700 text-white 
+        p-3  rounded-lg uppercase hover:opacity-95 
+        disabled:opacity-80'>{loading? 'Loading...' : 'Register'}</button>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Already have an account?</p>
+        <Link to='/'>
+          <span className='text-blue-700'>Sign in</span>
+        </Link>
+      </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
   )
 }
