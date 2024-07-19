613448c672fcdf55418a5cf4c1da34980a12dee5
nohinsabu01 <nohin.sabu@intern.mhtechin.com>
2024-07-19 11:27:53 +0530

    create pages and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from './pages/SignIn.jsx';
+import Register from './pages/Register.jsx';
+import Dashboard from './pages/Dashboard.jsx';
 
 export default function App() {
   return (
-    <h1 className='text-red-700'>App</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
