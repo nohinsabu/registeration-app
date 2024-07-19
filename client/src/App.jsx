f8dbe1e9c4bf7cdf3b007de2a9440e9887e3202e
nohinsabu01 <nohin.sabu@intern.mhtechin.com>
2024-07-19 17:43:22 +0530

    updating privateroute

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignIn from './pages/SignIn.jsx';
 import Register from './pages/Register.jsx';
 import Dashboard from './pages/Dashboard.jsx';
+import PrivateRoute from "./components/PrivateRoute.jsx";
 
 export default function App() {
   return (
@@ -9,7 +10,9 @@ export default function App() {
       <Routes>
         <Route path='/' element={<SignIn />} />
         <Route path='/register' element={<Register />} />
-        <Route path='/dashboard' element={<Dashboard />} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
